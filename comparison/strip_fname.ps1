0472b00642f303f0eb4dfab805436d72af85eb71
Juliette Cordor <professional@maybejules.com>
2024-05-20 21:12:26 +1000

    chore: removed sfsu

--- comparison/strip_fname.ps1
+++
@@ -1,3 +0,0 @@
-. $env:HOMEPATH\scoop\apps\scoop\current\lib\core.ps1
-
-strip_filename 'https://github.com/abbodi1406/vcredist/releases/download/v0.80.0/VisualCppRedist_AIO_x86_x64_80.zip'
--- src/output/colours.rs
+++
@@ -1,3 +0,0 @@
-#![allow(unused_imports)]
-
-include!(concat!(env!("OUT_DIR"), "/colours.rs"));
