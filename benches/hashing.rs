1ca4c1a4f47fa6c8146e668cba4b15963b88dd0d
Juliette Cordor <professional@maybejules.com>
2024-06-05 16:09:05 +1000

    chore: added large hash file functions

--- benches/hashing.rs
+++ benches/hashing.rs
@@ -19,6 +19,11 @@ $url = "https://github.com/ScoopInstaller/Main/releases/download/v$version/scoop
 $hash = "e2a1c7dd49d547fdfe05fc45f0c9e276cb992bd94af151f0cf7d3e2ecfdc4233"
 "#;
 
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 use criterion::{black_box, criterion_group, criterion_main, BatchSize, Criterion};
 
 #[inline(always)]
@@ -37,6 +42,46 @@ fn blake3_hash(input: impl AsRef<[u8]>) -> String {
     let mut hasher = blake3::Hasher::new();
 
     hasher.update(input.as_ref());
+
+    format!("{}", hasher.finalize())
+}
+
+#[inline(always)]
+fn sha256_hash_reader(mut input: impl BufRead) -> String {
+    use sha2::Digest;
+
+    let mut hasher = sha2::Sha256::new();
+
+    loop {
+        let data_len = if let Ok(data) = input.fill_buf() {
+            hasher.update(data);
+
+            data.len()
+        } else {
+            break;
+        };
+
+        input.consume(data_len);
+    }
+
+    format!("{:x}", hasher.finalize())
+}
+
+#[inline(always)]
+fn blake3_hash_reader(mut input: impl BufRead) -> String {
+    let mut hasher = blake3::Hasher::new();
+
+    loop {
+        let data_len = if let Ok(data) = input.fill_buf() {
+            hasher.update(data);
+
+            data.len()
+        } else {
+            break;
+        };
+
+        input.consume(data_len);
+    }
 
     format!("{}", hasher.finalize())
 }
@@ -68,23 +113,23 @@ fn criterion_benchmark(c: &mut Criterion) {
         )
     });
 
-    // const LARGE_FILE_PATH: &str = "benches/sfsu-x86_64-1.14.0-beta.1.exe";
+    const LARGE_FILE_PATH: &str = "benches/sfsu-x86_64-1.14.0-beta.1.exe";
 
-    // c.bench_function("sha256 hash large file", |b| {
-    //     b.iter_batched(
-    //         || std::fs::read(black_box(SMALL_FILE_PATH)).expect("could not read file"),
-    //         |file| sha256_hash(black_box(file)),
-    //         BatchSize::SmallInput,
-    //     )
-    // });
+    c.bench_function("sha256 hash large file", |b| {
+        b.iter_batched(
+            || BufReader::new(File::open(black_box(LARGE_FILE_PATH)).expect("could not read file")),
+            |file| sha256_hash_reader(black_box(file)),
+            BatchSize::SmallInput,
+        )
+    });
 
-    // c.bench_function("blake3 hash large file", |b| {
-    //     b.iter_batched(
-    //         || std::fs::read(black_box(SMALL_FILE_PATH)).expect("could not read file"),
-    //         |file| blake3_hash(black_box(file)),
-    //         BatchSize::SmallInput,
-    //     )
-    // });
+    c.bench_function("blake3 hash large file", |b| {
+        b.iter_batched(
+            || BufReader::new(File::open(black_box(LARGE_FILE_PATH)).expect("could not read file")),
+            |file| blake3_hash_reader(black_box(file)),
+            BatchSize::SmallInput,
+        )
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
