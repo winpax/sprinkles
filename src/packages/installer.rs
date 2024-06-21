a6c5e035d11f47cbfd901f0420858d1107b7658b
Juliette Cordor <professional@maybejules.com>
2024-06-21 17:32:22 +1000

    refactor: installers

--- src/packages/installer.rs
+++ src/packages/installer.rs
@@ -1,5 +1,9 @@
 //! Installer helpers
 
+use std::{future::IntoFuture, process::Output};
+
+use futures::FutureExt;
+
 use crate::{config, contexts::ScoopContext, packages::manifest::Installer, scripts};
 
 use super::models::manifest::{InstallerRunner, TOrArrayOfTs};
@@ -17,26 +21,44 @@ pub enum Error {
 /// Installer result type
 pub type Result<T, E = Error> = std::result::Result<T, E>;
 
-impl Installer {
+#[derive(Debug, Clone, PartialEq, Eq)]
+#[must_use]
+/// An installer host
+///
+/// This is used to run the installers
+pub struct InstallerHost<'ctx, C: ScoopContext<config::Scoop>> {
+    ctx: &'ctx C,
+    installer: Installer,
+    runner: InstallerRunner,
+}
+
+impl<'ctx, C: ScoopContext<config::Scoop>> InstallerHost<'ctx, C> {
+    /// Create a new installer host
+    pub fn new(ctx: &'ctx C, installer: Installer, runner: InstallerRunner) -> Self {
+        Self {
+            ctx,
+            installer,
+            runner,
+        }
+    }
+
     #[must_use]
-    /// Get the installer runner
-    pub fn get_runner(&self) -> Option<InstallerRunner> {
-        self.script
-            .clone()
-            .map(InstallerRunner::Script)
-            .or_else(|| self.file.clone().map(InstallerRunner::File))
+    /// Create a new installer host from an installer
+    pub fn from_installer(ctx: &'ctx C, installer: Installer) -> Option<Self> {
+        let runner = installer.get_runner()?;
+        Some(Self::new(ctx, installer, runner))
     }
 
     /// Run the installer
     ///
     /// # Errors
     /// - If the installer could not be run
-    pub async fn run(&self, ctx: &impl ScoopContext<config::Scoop>) -> Result<()> {
-        let runner = self.get_runner();
-        let args = self.args.clone().map(TOrArrayOfTs::to_vec);
+    pub async fn run(self) -> Result<Output> {
+        let runner = self.runner;
+        let args = self.installer.args.clone().map(TOrArrayOfTs::to_vec);
 
-        match runner {
-            Some(InstallerRunner::File(file)) => {
+        let output = match runner {
+            InstallerRunner::File(file) => {
                 let mut command = std::process::Command::new(file);
 
                 if let Some(ref args) = args {
@@ -45,12 +67,33 @@ impl Installer {
                     &mut command
                 }
                 .spawn()?
-                .wait()?;
+                .wait_with_output()?
             }
-            Some(InstallerRunner::Script(script)) => script.save(ctx)?.run().await?,
-            None => {}
-        }
+            InstallerRunner::Script(script) => script.save(self.ctx)?.run().await?,
+        };
 
-        Ok(())
+        Ok(output)
+    }
+}
+
+impl<'ctx, C: ScoopContext<config::Scoop>> IntoFuture for InstallerHost<'ctx, C> {
+    type Output = Result<Output>;
+
+    type IntoFuture =
+        std::pin::Pin<Box<dyn std::future::Future<Output = Self::Output> + Send + 'ctx>>;
+
+    fn into_future(self) -> Self::IntoFuture {
+        self.run().boxed()
+    }
+}
+
+impl Installer {
+    #[must_use]
+    /// Get the installer runner
+    pub fn get_runner(&self) -> Option<InstallerRunner> {
+        self.script
+            .clone()
+            .map(InstallerRunner::Script)
+            .or_else(|| self.file.clone().map(InstallerRunner::File))
     }
 }
