bd183c8538328d2e45d38026c3d9f48efa975633
Juliette Cordor <professional@maybejules.com>
2024-08-07 15:19:29 +1000

    added sha512 hash benchmark

--- benches/hashing.rs
+++ benches/hashing.rs
@@ -23,20 +23,40 @@ $hash = "e2a1c7dd49d547fdfe05fc45f0c9e276cb992bd94af151f0cf7d3e2ecfdc4233"
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-#[inline(always)]
-fn sha256_hash(input: impl AsRef<[u8]>) -> String {
-    use sha2::Digest;
+use sha2::Digest;
 
-    let mut hasher = sha2::Sha256::new();
+#[inline(always)]
+fn sha2_hash<D: Digest>(input: impl AsRef<[u8]>) -> String
+// WTF is this
+where
+    <D as sha2::digest::OutputSizeUser>::OutputSize: std::ops::Add,
+    <<D as sha2::digest::OutputSizeUser>::OutputSize as std::ops::Add>::Output:
+        sha2::digest::generic_array::ArrayLength<u8>,
+{
+    let mut hasher = D::new();
 
     hasher.update(input);
 
     format!("{:x}", hasher.finalize())
 }
 
+#[inline(always)]
+fn sha256_hash(input: impl AsRef<[u8]>) -> String {
+    sha2_hash::<sha2::Sha256>(input)
+}
+
+#[inline(always)]
+fn sha512_hash(input: impl AsRef<[u8]>) -> String {
+    sha2_hash::<sha2::Sha512>(input)
+}
+
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("sha256 hash script", |b| {
         b.iter(|| sha256_hash(black_box(SCRIPT)))
+    });
+
+    c.bench_function("sha512 hash script", |b| {
+        b.iter(|| sha512_hash(black_box(SCRIPT)))
     });
 }
 
