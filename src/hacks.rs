5a6a18740d6a4f89271b88a92aa70e840555fc62
Juliette Cordor <professional@maybejules.com>
2024-09-29 21:25:38 +1000

    fixed formatting

--- src/hacks.rs
+++ src/hacks.rs
@@ -1,4 +1,7 @@
-#![allow(clippy::single_component_path_imports, edition_2024_expr_fragment_specifier)]
+#![allow(
+    clippy::single_component_path_imports,
+    edition_2024_expr_fragment_specifier
+)]
 
 #[macro_use]
 mod hackros {
