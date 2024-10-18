a9189530ca8d4dc2ccef2c03a248a8a0907c6654
jewlexx <61040004+jewlexx@users.noreply.github.com>
2024-10-18 20:19:19 +1100

    implemented shim extension and shim boilerplate

--- src/shim.rs
+++ src/shim.rs
@@ -1,1 +1,41 @@
 //! Scoop shim helpers
+
+use std::fmt::Display;
+
+use quork::prelude::ListVariants;
+
+use crate::packages::reference::package::Reference;
+
+pub struct Shim {
+    name: String,
+    package: Reference,
+}
+
+impl Shim {
+    /// A list of all possible shim file path extensions
+    const SHIM_EXTENSIONS: [ShimExtension; 4] = ShimExtension::VARIANTS;
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, quork::macros::ListVariants)]
+/// All possible shim file path extensions
+enum ShimExtension {
+    /// Empty (no extension)
+    Empty,
+    /// .shim extension
+    Shim,
+    /// .cmd extension
+    Cmd,
+    /// .ps1 extension
+    Ps1,
+}
+
+impl Display for ShimExtension {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ShimExtension::Empty => Ok(()),
+            ShimExtension::Shim => Display::fmt(".shim", f),
+            ShimExtension::Cmd => Display::fmt(".cmd", f),
+            ShimExtension::Ps1 => Display::fmt(".ps1", f),
+        }
+    }
+}
