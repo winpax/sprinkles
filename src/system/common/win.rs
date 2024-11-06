f538d71be94d4520ef641494058b64bfa4da9995
Juliette Cordor <professional@maybejules.com>
2024-11-06 13:08:30 +1100

    fix issues with system windows implementations

--- src/system/common/win.rs
+++ src/system/common/win.rs
@@ -1,13 +1,13 @@
 use std::path::Path;
 
-pub struct Unix;
+pub struct Windows;
 
-impl super::Common for Unix {
+impl super::Common for Windows {
     fn symlink_dir(original: impl AsRef<Path>, link: impl AsRef<Path>) -> std::io::Result<()> {
-        std::os::windows::fs::symlink_dir(original, link)?;
+        std::os::windows::fs::symlink_dir(original, link)
     }
 
     fn symlink_file(original: impl AsRef<Path>, link: impl AsRef<Path>) -> std::io::Result<()> {
-        std::os::windows::fs::symlink_file(original, link)?;
+        std::os::windows::fs::symlink_file(original, link)
     }
 }
